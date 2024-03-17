dcc627369653055d5679db5c21b1439b839dd5d2
williamwmarx <m94@marx.sh>
2024-03-16 19:58:43 -0500

    Make clue numbers black by default

--- src/CrosswordGrid.tsx
+++ src/CrosswordGrid.tsx
@@ -20,7 +20,7 @@ const defaultTheme = {
   cellBackground: 'rgb(255,255,255)',
   cellBorder: 'rgb(0,0,0)',
   textColor: 'rgb(0,0,0)',
-  numberColor: 'rgba(0,0,0, 0.25)',
+  numberColor: 'rgb(0,0,0)',
   focusBackground: 'rgb(255,255,0)',
   highlightBackground: 'rgb(255,255,204)',
 };
--- src/CrosswordProvider.tsx
+++ src/CrosswordProvider.tsx
@@ -203,7 +203,7 @@ const defaultTheme: CrosswordProviderProps['theme'] = {
   cellBackground: 'rgb(255,255,255)',
   cellBorder: 'rgb(0,0,0)',
   textColor: 'rgb(0,0,0)',
-  numberColor: 'rgba(0,0,0,0.25)',
+  numberColor: 'rgb(0,0,0)',
   focusBackground: 'rgb(255,255,0)',
   highlightBackground: 'rgb(255,255,204)',
 };
